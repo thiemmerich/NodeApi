59d3f6dfd431f0837f23b6a0ed5716144f49badc
Thiago Emmerich <dev2@brametec.com.br>
2020-06-23 13:52:29 -0300

    Changes on ProductController's store method -> Create to Create or Update

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 module.exports = {
-  username: 'bc33814315d369',
-  password: 'b43598cc',
-  database: 'heroku_01c0aa3a553bb3f',
-  host: 'us-cdbr-east-05.cleardb.net',
+  username: 'user',
+  password: 'passwd',
+  database: 'database',
+  host: 'localhost',
   dialect: 'mysql',
   logging: (msg) => console.log(msg)
 }
-
-//mysql://bc33814315d369:b43598cc@us-cdbr-east-05.cleardb.net/heroku_01c0aa3a553bb3f?reconnect=true
