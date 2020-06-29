a9e91c486525a3c4f884067a51297e08646c3780
Thiago Emmerich <dev2@brametec.com.br>
2020-06-29 17:00:11 -0300

    Mudanças no tempo de duraçao do token de 300 para 18000 segundos

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  username: 'bc33814315d369',
-  password: 'b43598cc',
-  database: 'heroku_01c0aa3a553bb3f',
-  host: 'us-cdbr-east-05.cleardb.net',
+  username: 'user',
+  password: 'passwd',
+  database: 'scheme',
+  host: 'localhost',
   dialect: 'mysql',
   logging: (msg) => console.log(msg)
 }
