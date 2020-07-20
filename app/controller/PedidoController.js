99fe9f19899ed4ea6a40bd83c8293ee8bf15aeb1
Thiago Emmerich <dev2@brametec.com.br>
2020-07-20 14:15:14 -0300

    Adicionado o registro de novos pedidos com itens

--- app/controller/PedidoController.js
+++ app/controller/PedidoController.js
@@ -1,6 +1,8 @@
 const { Pedidos } = require('../models');
 const { PedidoItens } = require('../models');
 
+const db = require('../models/index');
+
 module.exports = {
 
     async show(req, res) {
@@ -9,26 +11,46 @@ module.exports = {
         return res.json(pedido);
     },
 
-    async gravarNovoPedido(idUsuario, valorTotal) {
-        const pedido = {
-            idUsuario: idUsuario,
-            valorTotal: valorTotal
-        }
+    async gravarNovoPedido(req, res) {
 
-        await Pedidos.create(pedido).then((pedido) => {
-            const itens = {
-                idProduto: 1,
-                idPedido: pedido.idPedido,
-                tamanho: 'M',
-                quantidade: 2,
-                valorTotal: 50.00
-            }
+        const pedido = req.body.pedido;
+        const itens = req.body.itens;
+        //console.log("ITENS:");
+        //console.log(itens);
+        //console.log("BODY:");
+        //console.log(req.body);
 
-            PedidoItens.create(itens).then((itens) => {
-                return itens;
-            }).catch(() => {
-                return pedido;
+        const transacao = await db.sequelize.transaction();
+
+        await Pedidos.create(pedido, { transaction: transacao })
+            .then(async (pedido) => {
+
+                itens.forEach(element => {
+
+                    element.idPedido = pedido.idPedido;
+                });
+
+                PedidoItens.bulkCreate(itens, { transaction: transacao })
+                    .then(async (item) => {
+                        console.log('OK: ' + item);
+                        await transacao.commit();
+                    })
+                    .catch(async (err) => {
+                        await transacao.rollback();
+
+                        return res.status(400).json({
+                            errorMsg: 'ERRO: Insercao de itens do pedido! -> ' + err
+                        });
+                    });
+
+                return res.json([pedido, itens]);
+            })
+            .catch(async (err) => {
+                await transacao.rollback();
+
+                return res.status(400).json({
+                    errorMsg: 'ERRO: Insercao de pedido! -> ' + err
+                });
             });
-        });
     },
 };
